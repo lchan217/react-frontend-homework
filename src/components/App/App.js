b83e897bd66e4137c9645a73e8a9e45fee257fba
Lizzie <lchan217@gmail.com>
2021-05-01 09:49:04 -0600

    fix searching by name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,16 +7,19 @@ import Errors from '../Errors/Errors'
 
 const App = () => {
     const [hotels, setHotels] = useState([]);
+    const [filteredHotels, setFilteredHotels] = useState([])
     const [errors, setErrors] = useState('');
     const [searchInput, setSearchInput] = useState('');
 
     useEffect(() => {
         if(hotels && searchInput){
-            const filteredHotels = hotels.filter(hotel => {
-                hotel.hotelStaticContent.name.toLowerCase().includes(searchInput.toLowerCase())
-            })
-            // TODO: filteredHotels always is an empty array
-            setHotels(filteredHotels)
+            let result = []
+            if(filteredHotels.length){
+                result =  filteredHotels.filter(hotel => hotel.hotelStaticContent.name.toLowerCase().includes(searchInput.toLowerCase()))
+            } else {
+                result = hotels.filter(hotel => hotel.hotelStaticContent.name.toLowerCase().includes(searchInput.toLowerCase()))
+            }
+            setFilteredHotels(result)
         } else {
             hotelResultService.get().then(response => {
                 setHotels(response.results.hotels)
@@ -45,15 +48,17 @@ const App = () => {
                             onChange={handleSearchChange}
                             className="input" />
                         Price
-                        <select name="" className="select">
-                            <option value="">Recommended</option>
-                            <option value="">Price low-to-high</option>
-                            <option value="">Price high-to-low</option>
+                        <select name="" className="select" >
+                            <option value="recommended">Recommended</option>
+                            <option value="low-to-high">Price low-to-high</option>
+                            <option value="high-to-low">Price high-to-low</option>
                         </select>
                         <button className="button">Reset</button>
                     </div>
                 </div>
-            {errors ? <Errors errors={errors} /> : <HotelList hotels={hotels} />}
+            {errors ? <Errors errors={errors} /> : 
+                filteredHotels.length ? <HotelList hotels={filteredHotels} />: 
+                <HotelList hotels={hotels} />}
             
             </div>
         </div>
